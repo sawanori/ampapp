fcdcd7d50672729b75dda76973e1e90842a6fcf3
sawadanoritaka <snp.inc.info@gmail.com>
2021-04-12 18:33:31 +0900

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 const App = () => {
   return (
     <div>
       <h1>テスト</h1>
+      <AmplifySignOut />
     </div>
   )
 }
